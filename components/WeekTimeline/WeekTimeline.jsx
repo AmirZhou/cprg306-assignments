5d1ce5d54849a67e31417c9fbcaea428c299d34d
Amir Yue Zhou <amirzhou001@gmail.com>
2025-01-21 17:51:11 -0700

    implements horizontal scroll, need to fix not fully scroll

--- components/WeekTimeline/WeekTimeline.jsx
+++ components/WeekTimeline/WeekTimeline.jsx
@@ -1,14 +1,36 @@
-import WeekCard from './WeekCard';
+'use client';
+import { useRef, useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Link from 'next/link';
+import WeekCard from './WeekCard';
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function WeekTimeline() {
+  const timelineRef = useRef(null);
+
+  useEffect(() => {
+    const element = timelineRef.current;
+    gsap.to(element, {
+      x: () => -(element.scrollWidth - element.offsetWidth),
+      ease: 'none',
+      scrollTrigger: {
+        trigger: element,
+        markers: true,
+        start: 'top 0',
+        end: '+=2000px',
+        scrub: true,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+  }, []);
+
   const positions = ['items-center', 'items-end', 'items-start']; // Full class names for vertical alignment
+
   return (
-    <div className="overflow-hidden h-[700px] border flex py-24">
+    <div className="h-[700px] border flex py-24" ref={timelineRef}>
       {Array.from({ length: 5 }).map((_, index) => (
         <div
           key={index}
