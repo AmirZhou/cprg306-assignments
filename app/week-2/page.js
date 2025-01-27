8c06604ba8039f8ef87cf330dcd704370b45815f
Amir Yue Zhou <amirzhou001@gmail.com>
2025-01-27 16:45:32 -0700

    feat: improve student-info design

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,10 +1,13 @@
 import StudentInfo from './student-info';
+import { dotenv } from 'dotenv';
 
 export default function Page() {
   return (
-    <main>
-      <h1>Shopping Listss</h1>
-      <StudentInfo />
-    </main>
+    <div className="in-w-screen min-h-screen flex flex-col gap-4 bg-slate-700">
+      <h1 className="text-slate-300 text-4xl">Shopping List</h1>
+      <main>
+        <StudentInfo name={"Yue (Amir) Zhou"} repoAddress={process.env.REPO_ADDRESS} />
+      </main>
+    </div>
   );
 }
