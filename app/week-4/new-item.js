7311a5023043bdf4c1842b4f924ffeba98ce5ed8
Amir Yue Zhou <amirzhou001@gmail.com>
2025-02-04 14:06:28 -0700

    feat: implements week-4 core function (without animation)

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,8 +1,9 @@
 "use client";
 import { useState, useCallback } from "react";
+import { PlusIcon, MinusIcon } from "@/components/Icons";
 
 export default function NewItem() {
-  const { quantity, setQuantity } = useState(1);
+  const [quantity, setQuantity] = useState(1);
 
   const increment = useCallback(() => {
     setQuantity(prev => prev < 20 ? prev + 1 : prev);
@@ -10,14 +11,19 @@ export default function NewItem() {
   }, []);
 
   const decrement = useCallback(() => {
-    setQuantity(prev => prev > 0 ? prev - 1 : prev);
+    setQuantity(prev => prev > 1 ? prev - 1 : prev);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div className="">
-    <span></span>
-    <span>{quantity}</span>
-    <span></span>
-    </div>
+  return (
+    <div className="flex w-24 mx-auto justify-between ">
+      <span>
+        <MinusIcon onClick={decrement} />
+      </span>
+      <span className="w-8 text-center">{quantity}</span>
+      <span>
+        <PlusIcon onClick={increment} />
+      </span>
+    </div>);
 }
 
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,8 +1,9 @@
 import NewItem from "./new-item"
 
 export default function Page() {
-  return
-  <div className="w-screen min-h-screen p-2 flex flex-col justify-center items-cente bg-slate-700r">
-    <NewItem />
-  </div>
+  return (
+    <div className="w-screen min-h-screen p-2 flex flex-col justify-center items-cente bg-slate-700r">
+      <NewItem />
+    </div>
+  )
 }
