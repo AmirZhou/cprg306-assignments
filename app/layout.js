cf6adcb2e26b55471db4cdb5a9b82704859d3ec5
Amir Yue Zhou <amirzhou001@gmail.com>
2025-03-07 13:59:25 -0700

    fix: fix horizontal scroll bar

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased w-full`}
       >
         {children}
       </body>
