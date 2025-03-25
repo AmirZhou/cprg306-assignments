83f7767a9fc607b602aa26938605b5ea14e55190
Yue Amir Zhou <amirzhou001@gmail.com>
2025-03-25 07:26:36 -0600

    feat: header-auth component

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -1,1 +1,57 @@
 import { signOutAction } from "@/app/(auth-pages)/actions";
+import { createClient } from "@/utils/supabase/server";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+
+export default async function HeaderAuth() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!hasEnvVars)
+    return (
+      <div className="flex gap-2">
+        <Button
+          asChild
+          size="sm"
+          variant="outline"
+          disabled
+          className="pointer-events-none cursor-none opacity-75"
+        >
+          <Link href="/sign-in">Sign in</Link>
+        </Button>
+        <Button
+          asChild
+          size="sm"
+          variant="default"
+          disabled
+          className="pointer-events-none cursor-none opacity-75"
+        >
+          <Link href="/sign-in">Sign up</Link>
+        </Button>
+      </div>
+    );
+
+  return user ? (
+    <div className="flex items-center gap-4">
+      Hello, {user.email}!
+      <form action={signOutAction}>
+        <Button type="submit" variant="outline">
+          Sign out
+        </Button>
+      </form>
+    </div>
+  ) : (
+    <div className="flex gap-2">
+      <Button asChild variant="outline" size="sm">
+        <Link href="/sign-in">Sign in</Link>
+      </Button>
+      <Button asChild variant="default" size="sm">
+        <Link href="/sign-up">Sign up</Link>
+      </Button>
+    </div>
+  );
+}
