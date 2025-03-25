ded2130a97ed2adcbb748cb831978b9682b168f0
Yue Amir Zhou <amirzhou001@gmail.com>
2025-03-24 23:59:01 -0600

    feat: create secured route

--- app/week-9/secure/page.js
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div className="w-full min-h-screen flex items-start pt-20 justify-center bg-slate-50 font-poppins">
-      <h1 className="text-4xl">Week 9</h1>
-      <p>Only logged in users can see this.</p>
-    </div>
-  )
-}
---
+++ app/week-9/secure/page.tsx
@@ -0,0 +1,18 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const supabase = await createClient();
+  const { data: user } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+
+  return (
+    <div className="flex min-h-screen w-full items-start justify-center bg-slate-50 pt-20 font-poppins">
+      <h1 className="text-4xl">Week 9</h1>
+      <p className="text-green-800">Only logged in users can see this.</p>
+    </div>
+  );
+}
