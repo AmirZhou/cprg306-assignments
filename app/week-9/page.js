6d149480b60520d7d3b765f027ad785c7ed8a942
Yue Amir Zhou <amirzhou001@gmail.com>
2025-03-25 11:53:52 -0600

    feat: improve week-9 root page

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,9 +1,9 @@
 
 export default function Page() {
   return (
-    <div className="w-full gap-2 flex flex-col font-poppins">
-      <h1 className="text-4xl">Week 9</h1>
-      <p>not protected page</p>
+    <div className="w-full gap-4 flex flex-col h-full font-poppins pt-16">
+      <h1 className="text-6xl text-center font-bold">Cprg-306/ week-9</h1>
+      <p className="text-center text-lg">The course we love.</p>
     </div>
   )
 }
