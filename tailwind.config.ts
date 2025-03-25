30932a663efe1192c543acc4fd2d2406c2b9441e
Yue Amir Zhou <amirzhou001@gmail.com>
2025-03-24 18:51:25 -0600

    fix: updata tailwind config file from mjx to ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -45,8 +46,10 @@ export default {
       },
       fontFamily: {
         poppins: 'var(--font-poppins)',
-      }
+      },
     },
   },
   plugins: [],
 };
+
+export default config;
